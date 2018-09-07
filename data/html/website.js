767e2c9d14622ed136d427e9712327cc7d4f0bf3
yang498 <1434117925@qq.com>
2018-09-07 18:03:25 +0800

    更新数据

--- data/html/website.js
+++ data/html/website.js
@@ -43,8 +43,12 @@ commonData.html.website = {
 
 	#远程连接工具
 	##SecureCRT
-	百度 SecureCRT 下载，完成后安装x64位的，一直点下一步
+	命令式
+	百度 SecureCRT 下载或直接在电脑管家的软件管理中也可以（官网下载还要登录），一直点下一步即可
 	再百度 SecureCRT keygen 下载注册机，比如在@[多多软件站|https://www.baidu.com/link?url=DziWjZR39cXFht1SnuhS1RygelmH0W0cW4LbohLmLub2Oiqi2pEfsjsnfPI-3MBgD6csTQe5_-WNrfvHaRGZPq&wd=&eqid=e6a36eba0002a1b0000000035adc5254]下载了一个，在安装的时候注意把附带其他的软件的勾给去掉，不然你懂得，超烦的，运行 SecureCRT keygen.exe，点击左下的 patch，选择安装路径的 SecureCRT.exe，第二次选择 LicenseHelper.exe，成功后点击 Generate 生成许可证，再打开 SecureCRT，第一个大的输入框不填点下一步，对应的框复制粘贴对应的名称、序列号、日期即可，最后的大输入框也不填就可以了
+	##WinSCP
+	文件式
+	官网下载即可
 
 	@@
 	阿里云-文档|https://help.aliyun.com/?spm=a2c4g.11186623.201511181.1.EnHOJL
