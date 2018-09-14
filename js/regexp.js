80d2e2946442445bcacefe3632c11fb1bff7aaa2
yang498 <1434117925@qq.com>
2018-09-14 17:13:11 +0800

    更新数据

--- js/regexp.js
+++ js/regexp.js
@@ -5,11 +5,11 @@ let REG_UN = {
 	// 开头声明，粉
 	statement: 'var|let |const|void|function|=&gt;|new |class\s|constructor|super|static|import|export |default',
 	// 循环分支，紫
-	loopFork: 'for | in |of\s|while|\sdo|if |else |switch|case|break|continue|try\s|catch|finally|with',
+	loopFork: 'for | in |of\s|while|\sdo|if |else | switch|case|break|continue|try\s|catch|finally|with',
 	// 方法关键字，蓝
 	methodKeyword: 'return|delete|typeof|require\s|throw|eval|instanceof|debugger|this|length',
 	// 类型方法，紫
-	type: 'window|document|console|true|false|undefined|null|Object|Array|Boolean|String|Number|Math|Date|RegExp|Error|JSON'
+	type: 'window|document|console|true|false|undefined|null| Object| Array| Boolean| String| Number|Math|Date|RegExp|Error|JSON'
 }
 
 // 将 REG 加上 (?!¿)，用来匹配
