eae7d8253d04d6b46aa96bdd95869ffbadef221a
yang498 <1434117925@qq.com>
2020-04-28 00:29:50 +0800

    update

---
+++ src/pages/404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="tc" :class="$store.state.skin">
+        <v-header></v-header>
+        <h1>404</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        VHeader: () => import('P/index/VHeader')
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    font-size: 100px;
+    margin-top: 100px;
+}
+</style>
